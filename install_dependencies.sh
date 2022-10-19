7a8f433c64bb3aebefeb10a4bb752489e8182792
Sharmitha Ganesan M.Eng (Robotics) <sharmithaganesan08@gmail.com>
2022-10-19 15:04:25 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,9 @@
 # Install lcov for code coverage
 sudo apt-get install -y -qq lcov
 
-# OpenCV dependencies - Details available at: http://docs.opencv.org/trunk/doc/tutorials/introduction/linux_install/linux_install.html
+# OpenCV dependencies
+sudo apt update
+sudo apt upgrade
 sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install python3.8-dev python3-numpy libtbb2 libtbb-dev
 sudo apt-get install libjpeg-dev libpng-dev libtiff5-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
