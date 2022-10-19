3dbaabd7b134fd3aa43388ab6fb93ca6ac4b90f7
Sharmitha Ganesan M.Eng (Robotics) <sharmithaganesan08@gmail.com>
2022-10-19 14:57:16 -0400

    Update yolo_dependency.sh

--- yolo_dependency.sh
+++ yolo_dependency.sh
@@ -1,2 +1,2 @@
 wget -O yolov5s.onnx 'https://drive.google.com/file/d/18VVsEZpAaaVoFDLJcHQhlsf2Dalc-Bys/view?usp=sharing'  --no-check-certificate
-mv yolov5s.onnx yolov5s/yolov5s.onnx
+mv yolov5s.onnx /yolov5s/yolov5s.onnx
