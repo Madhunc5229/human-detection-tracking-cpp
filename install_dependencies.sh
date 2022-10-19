1c08af04b5a4383f5d7d01bfb6961d38c2efee9b
Sharmitha Ganesan M.Eng (Robotics) <sharmithaganesan08@gmail.com>
2022-10-19 12:59:00 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,7 +6,7 @@ sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcod
 sudo apt-get install python3.8-dev python3-numpy libtbb2 libtbb-dev
 sudo apt-get install libjpeg-dev libpng-dev libtiff5-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
 sudo su
-git clohttps://github.com/Madhunc5229/human-detection-tracking-cpp/actions/runs/3283333369/jobs/5407912887ne https://github.com/Itseez/opencv.git
+git clone https://github.com/Itseez/opencv.git
 git clone https://github.com/Itseez/opencv_contrib.git
 cd opencv
 mkdir release && cd release
