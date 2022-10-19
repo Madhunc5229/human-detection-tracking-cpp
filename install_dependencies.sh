0dc7863559ea2cd3c7b987dab303ef2eaab6f263
Sharmitha Ganesan M.Eng (Robotics) <sharmithaganesan08@gmail.com>
2022-10-19 12:29:42 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,35 +1,19 @@
 # Install lcov for code coverage
 sudo apt-get install -y -qq lcov
 
-# Install Eigen
-sudo apt install libeigen3-dev
-sudo apt-get install -qq g++-7
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
-
 # OpenCV dependencies - Details available at: http://docs.opencv.org/trunk/doc/tutorials/introduction/linux_install/linux_install.html
-sudo apt-get install -y build-essential
-sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python3.8-dev python3-numpy libtbb2 libtbb-dev
+sudo apt-get install libjpeg-dev libpng-dev libtiff5-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
+sudo su
+/opt $ git clone https://github.com/Itseez/opencv.git
+/opt $ git clone https://github.com/Itseez/opencv_contrib.git
+cd opencv
+mkdir release && cd release
+cmake -D BUILD_TIFF=ON -D WITH_CUDA=OFF -D ENABLE_AVX=OFF -D WITH_OPENGL=OFF -D WITH_OPENCL=OFF -D WITH_IPP=OFF -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_EIGEN=OFF -D WITH_V4L=OFF -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
+make -j4
+make install
+ldconfig
+exit
 
-sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-
-wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.1.zip
-wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.1.zip
-unzip opencv.zip
-unzip opencv_contrib.zip
-
-cd opencv-4.5.1
-mkdir build
-cd build
-
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_EXAMPLES=OFF -D BUILD_opencv_apps=OFF -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D OPENCV_ENABLE_NONFREE=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.5.1/modules -D CMAKE_INSTALL_PREFIX=/usr/local ..
-
-make -j$(nproc)
-
-sudo make install
-
-sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
-sudo ldconfig
-cd ../../
-sudo rm -rf opencv-4.5.1
-sudo rm -rf opencv_contrib-4.5.1
 echo "OpenCV installed."
