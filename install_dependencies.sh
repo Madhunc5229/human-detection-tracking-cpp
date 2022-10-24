2c4ddbe031772a3f0498457f0c0376526f20e75f
Sharmitha Ganesan M.Eng (Robotics) <sharmithaganesan08@gmail.com>
2022-10-23 20:08:30 -0400

    Updating py version in dev

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y -qq lcov
 sudo apt update
 sudo apt upgrade
 sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install python3.8-dev python3-numpy libtbb2 libtbb-dev
+sudo apt-get install python3.9-dev python3-numpy libtbb2 libtbb-dev
 sudo apt-get install libjpeg-dev libpng-dev libtiff5-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
 sudo su
 cd /opt
